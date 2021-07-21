f6a17b706c3f668be32f0aa02be5f398d1a95918
Shamel Lakin <christlife4u@gmail.com>
2021-07-21 08:37:00 -0400

    js

--- keyboard/Keyboard.js
+++ keyboard/Keyboard.js
@@ -36,7 +36,7 @@ const Keyboard = {
     // Automatically use keyboard for elements with .use-keyboard-input
     document.querySelectorAll(".use-keyboard-input").forEach((element) => {
       element.addEventListener("focus", () => {
-        // this.open(element.value, (currentValue) => {
+        this.open(element.value, (currentValue) => {
           element.value = currentValue;
         });
       });
